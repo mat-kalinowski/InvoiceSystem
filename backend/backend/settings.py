096649970a0311b03db393bdf6184fe18690e27d
Mateusz Kalinowski <mateusz.kalinowski@arm.com>
2023-03-26 14:54:12 +0200

    Connect PostreSQL database with backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test",
+        "USER": "test",
+        "PASSWORD": "test",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
